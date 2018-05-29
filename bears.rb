4d5c6740e9361fa13ffc917f824ebed68aa70a9b
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-05-29 19:54:46 +0100

    fish from river to bear tests pass

--- bears.rb
+++ bears.rb
@@ -8,4 +8,11 @@ class Bear
     @stomach_contents = stomach_contents
   end
 
+  def takes_fish_from_river(river)
+    random_number = rand(river.fish_in_river.count)
+    random_fish = river.fish_in_river[random_number]
+    @stomach_contents.push(random_fish)
+    river.fish_in_river.delete(random_fish)
+  end
+
 end
--- specs/bears_spec.rb
+++ specs/bears_spec.rb
@@ -1,10 +1,12 @@
 require("minitest/autorun")
 require_relative("../bears.rb")
+require_relative("../rivers.rb")
 
 class BearsTest < MiniTest::Test
 
   def setup()
     @yogi_bear = Bear.new("Yogi", "Grizzly")
+    @amazon = River.new("Amazon", ["Fingers", "Cake", "n Chips", "Pi", "Ee", "Ing"])
   end
 
   def test_bear_name
@@ -17,6 +19,12 @@ class BearsTest < MiniTest::Test
 
   def test_bear_stomach_contents
     assert_equal(0, @yogi_bear.stomach_contents.count)
+  end
+
+  def test_bear_takes_fish_from_river
+    @yogi_bear.takes_fish_from_river(@amazon)
+    assert_equal(1, @yogi_bear.stomach_contents.count)
+    assert_equal(5, @amazon.fish_in_river.count)
   end
 
 end
